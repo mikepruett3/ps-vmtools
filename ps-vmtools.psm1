1ad19659ee780ad1b364643b88a1f3357f16d601
Mike Pruett <mikepruett3@gmail.com>
2024-07-29 15:10:31 -0500

    Removed Verbose Tag!

--- ps-vmtools.psm1
+++ ps-vmtools.psm1
@@ -1,7 +1,7 @@
 #Requires -Modules VMware.VIM
 
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\ -Filter *.ps1 -ErrorAction SilentlyContinue )
-Import-Module -Name $PSScriptRoot\Public\*.ps1 -Function * -Verbose -ErrorAction SilentlyContinue
-Import-Module -Name $PSScriptRoot\Private\*.ps1 -Function * -Verbose -ErrorAction SilentlyContinue
+Import-Module -Name $PSScriptRoot\Public\*.ps1 -Function * -ErrorAction SilentlyContinue
+Import-Module -Name $PSScriptRoot\Private\*.ps1 -Function * -ErrorAction SilentlyContinue
 
 Export-ModuleMember -Function $Public.BaseName
